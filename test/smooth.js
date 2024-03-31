71dc9219ffaf2aef516c51f608eeed7e2d4a2287
Valentin-BEAUSSAERT <valentin.beaussaert@laplateforme.io>
2024-03-31 22:27:00 +0100

    ajout de la section 3 plus modification du css avec des identifiant

--- test/smooth.js
+++ test/smooth.js
@@ -1,29 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let throttleTimeout;
-  const sections = document.querySelectorAll('.background');
-  let currentSectionIndex = 0;
-  const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+    let throttleTimeout;
+    const sections = document.querySelectorAll('.background');
+    let currentSectionIndex = 0;
 
-  const scrollToSection = (index) => {
-      if (index >= 0 && index < sections.length) {
-          sections[index].scrollIntoView({ behavior: 'smooth', block: 'start' });
-          currentSectionIndex = index;
-      }
-  };
+    const scrollToSection = (index) => {
+        if (index >= 0 && index < sections.length) {
+            sections[index].scrollIntoView({ behavior: 'smooth', block: 'start' });
+            currentSectionIndex = index;
+        }
+    };
 
-  const throttleScroll = (e) => {
-      if (isMac) {
-          e.preventDefault(); // Sur Mac, cela empêche le défilement standard pour un meilleur contrôle
-      }
-      if (throttleTimeout) return; // Empêche l'exécution répétée
+    const throttleScroll = (e) => {
+        if (throttleTimeout) return; // Empêche l'exécution répétée
 
-      throttleTimeout = setTimeout(() => {
-          throttleTimeout = null;
-          const direction = e.deltaY > 0 ? 1 : -1; // Détermine la direction du défilement
-          scrollToSection(currentSectionIndex + direction);
-      }, isMac ? 500 : 150); // Sur Mac, augmentez le délai pour ralentir le défilement
-  };
+        throttleTimeout = setTimeout(() => {
+            throttleTimeout = null;
+            const direction = e.deltaY > 0 ? 1 : -1; // Détermine la direction du défilement
+            scrollToSection(currentSectionIndex + direction);
+        }, 150); // Utilisez un délai constant pour simplifier
+    };
 
-  // Ajoutez ici `{ passive: false }` pour permettre `e.preventDefault()` sans avertissement
-  document.addEventListener('wheel', throttleScroll, { passive: false });
+    // Suppression de la condition spécifique à Mac pour unifier le comportement
+    document.addEventListener('wheel', (e) => {
+        e.preventDefault(); // Empêche le défilement standard pour un contrôle uniforme
+        throttleScroll(e);
+    }, { passive: false }); // `{ passive: false }` permet `e.preventDefault()` sans avertissement
 });
