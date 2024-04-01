07354b3c17df770e51c7ee3e29d0e10f439cd960
SK4 <valentin.beaussaert@laplateforme.io>
2024-04-01 11:44:47 +0200

    réglage du smooth plus début du responsive

--- test/smooth.js
+++ test/smooth.js
@@ -1,22 +1,26 @@
 function smoothScrollTo(element, duration) {
-    let start = window.scrollY,
-        end = element.getBoundingClientRect().top,
-        startTime = null;
+    let start = window.pageYOffset;
+    let end = element.getBoundingClientRect().top + window.pageYOffset;
+    let change = end - start;
+    let startTime = null;
 
     function scroll(timestamp) {
         if (!startTime) startTime = timestamp;
         let progress = timestamp - startTime;
-        let position = start + (end * (progress / duration));
-
-        window.scrollTo(0, position);
+        let position = start + change * (progress / duration);
 
         if (progress < duration) {
+            window.scrollTo(0, position);
             window.requestAnimationFrame(scroll);
+        } else {
+            // S'assurer que nous allons exactement à la fin du scroll prévu
+            window.scrollTo(0, end);
         }
     }
 
     window.requestAnimationFrame(scroll);
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll('.background');
@@ -26,12 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const scrollToSection = (index) => {
         if (index >= 0 && index < sections.length && !isScrolling) {
             isScrolling = true;
-            smoothScrollTo(sections[index], 500); // 1000ms = 1 seconde pour le défilement
+            smoothScrollTo(sections[index], 800); // 1000ms = 1 seconde pour le défilement
             currentSectionIndex = index;
 
             setTimeout(() => {
                 isScrolling = false;
-            }, 1000); // Assumer que le défilement prend 1 seconde
+            }, 800); // Assumer que le défilement prend 1 seconde
         }
     };
 
